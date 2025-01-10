dd928b003e9a4481cd3f5cb8c9d990b331ee8597
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-01-10 13:09:59 -0500

    Initial commit, added a DB Connection

---
+++ config/dbconfig.js
@@ -0,0 +1,16 @@
+
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+);
+
+
+
+export default sequelize // an instance of SQLize we create in the index.js file
